f918f405c966f17ab32303e6561ac37f21960b0f
sourabh27 <sourabh@chat360.io>
2024-04-17 15:41:10 +0530

    authentication

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,5 +1,56 @@
+import axios from "axios";
+import { FormEvent } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+
 export default function Login() {
+    const navigate = useNavigate();
+
+    const [userData, setUserData] = useState({
+        email: '',
+        password: '',
+    })
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        console.log('Logging in...');
+
+        try {
+            const formData = new FormData();
+            formData.append('username', userData.email);
+            formData.append('password', userData.password);
+            
+            console.log(userData);
+            axios.post("http://localhost:8000/v1/token", formData, {
+                headers: {
+                "Content-Type": "multipart/form-data",
+                    },
+            })
+            .then((response) => {
+                console.log("response", response.data);
+                if (response.status === 200) {
+                    localStorage.setItem('token', response.data.access_token);
+                    navigate('/');
+                }
+            })
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
     return (
-        <h1>Login</h1>
-    );
+    <>
+    <div className="my-48 bg-gray-200 py-8">
+        <h1 className="text-2xl text-center font-bold p-4">Login</h1>
+        <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+            <input type="email" onChange={(event) => setUserData({...userData, email: event.target.value})} placeholder="Email" name="username" className="px-8 py-2 mt-4 rounded-md" />
+            <input type="password" onChange={(event) => setUserData({...userData, password: event.target.value})} placeholder="Password" name="password" className="px-8 py-2 my-4 rounded-md" />
+            <button className="bg-teal-500 text-white px-4 py-2 rounded-md">Login</button>
+        </form>
+    </div>
+    </>
+    )
 }
--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -1,7 +1,46 @@
+import { FormEvent } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 
 function Register() {
+    const navigate = useNavigate();
+    
+    const [formData, setFormData] = useState({
+        full_name: "",
+        email: "",
+        password: ""
+    });
+
+    function handleSubmit(event: FormEvent){
+        event.preventDefault();
+        console.log("Form Submitted");
+        console.log(formData);
+
+        try {
+            axios.post('http://127.0.0.1:8000/v1/users', formData)
+            .then((response) => {
+                console.log("response", response.data);
+                if (response.status === 201) {
+                navigate('/login');
+                }
+            })
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
-        <h1 className="text-green-500">Register</h1>
+        <div className="my-48 bg-gray-200 py-8">
+            <h1 className="text-2xl text-center text-teal-500 font-bold mb-4">Register</h1>
+            <form className="flex flex-col items-center gap-4" onSubmit={handleSubmit}>
+                <input onChange={(event) => setFormData({...formData, full_name: event.target.value})} type="text" placeholder="Full Name" className="px-8 py-2 rounded-md" />
+                <input onChange={(event) => setFormData({...formData, email: event.target.value})} type="email" placeholder="Email" className="px-8 py-2 rounded-md" />
+                <input onChange={(event) => setFormData({...formData, password: event.target.value})} type="password" placeholder="Password" className="px-8 py-2 rounded-md" />
+                <button className="px-8 py-2 bg-teal-500 text-white rounded-md">Register</button>
+            </form>
+        </div>
     );
 }
 
